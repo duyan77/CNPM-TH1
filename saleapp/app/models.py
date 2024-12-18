46076b1f60646351359aa61f8feadefbd3be6661
duyan77 <fklt0961@gmail.com>
2024-12-18 15:17:04 +0700

    a

--- saleapp/app/models.py
+++ saleapp/app/models.py
@@ -19,7 +19,8 @@ class User(db.Model, UserMixin):
     username = Column(String(100), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
     avatar = Column(String(100),
-                    default="https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg")
+                    default="https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735"
+                            "/cg6clgelp8zjwlehqsst.jpg")
     user_role = Column(Enum(UserRole), default=UserRole.USER)
     receipts = relationship('Receipt', backref='user', lazy=True)
     comments = relationship('Comment', backref='user', lazy=True)
@@ -73,66 +74,66 @@ class Comment(db.Model):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        # db.create_all()
 
 
 
-        # u = User(name='admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
-        #          user_role=UserRole.ADMIN)
-        # db.session.add(u)
-        # db.session.commit()
-        #
-        # c1 = Category(name='Mobile')
-        # c2 = Category(name='Tablet')
-        # c3 = Category(name='Desktop')
-        #
-        # db.session.add_all([c1, c2, c3])
-        # db.session.commit()
-        #
-        # data = [{
-        #     "name": "iPhone 7 Plus",
-        #     "description": "Apple, 32GB, RAM: 3GB, iOS13",
-        #     "price": 17000000,
-        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
-        #     "category_id": 1
-        # }, {
-        #     "name": "iPad Pro 2020",
-        #     "description": "Apple, 128GB, RAM: 6GB",
-        #     "price": 37000000,
-        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
-        #     "category_id": 2
-        # }, {
-        #     "name": "iPad Pro 2021",
-        #     "description": "Apple, 128GB, RAM: 6GB",
-        #     "price": 37000000,
-        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
-        #     "category_id": 2
-        # }, {
-        #     "name": "iPad Pro 2022",
-        #     "description": "Apple, 128GB, RAM: 6GB",
-        #     "price": 37000000,
-        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
-        #     "category_id": 2
-        # }, {
-        #     "name": "iPad Pro 2023",
-        #     "description": "Apple, 128GB, RAM: 6GB",
-        #     "price": 37000000,
-        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
-        #     "category_id": 2
-        # }, {
-        #     "name": "iPad Pro 2024",
-        #     "description": "Apple, 128GB, RAM: 6GB",
-        #     "price": 37000000,
-        #     "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
-        #     "category_id": 2
-        # }]
-        #
-        # for p in data:
-        #     prod = Product(name=p['name'] + ' ' + str(random.randint(1, 100)), description=p['description'], price=p['price'],
-        #                    image=p['image'], category_id=p['category_id'])
-        #     db.session.add(prod)
-        #
-        # db.session.commit()
+        u = User(name='admin', username='admin', password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()),
+                 user_role=UserRole.ADMIN)
+        db.session.add(u)
+        db.session.commit()
+
+        c1 = Category(name='Mobile')
+        c2 = Category(name='Tablet')
+        c3 = Category(name='Desktop')
+
+        db.session.add_all([c1, c2, c3])
+        db.session.commit()
+
+        data = [{
+            "name": "iPhone 7 Plus",
+            "description": "Apple, 32GB, RAM: 3GB, iOS13",
+            "price": 17000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg",
+            "category_id": 1
+        }, {
+            "name": "iPad Pro 2020",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
+            "category_id": 2
+        }, {
+            "name": "iPad Pro 2021",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
+            "category_id": 2
+        }, {
+            "name": "iPad Pro 2022",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
+            "category_id": 2
+        }, {
+            "name": "iPad Pro 2023",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
+            "category_id": 2
+        }, {
+            "name": "iPad Pro 2024",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1690528735/cg6clgelp8zjwlehqsst.jpg",
+            "category_id": 2
+        }]
+
+        for p in data:
+            prod = Product(name=p['name'] + ' ' + str(random.randint(1, 100)), description=p['description'], price=p['price'],
+                           image=p['image'], category_id=p['category_id'])
+            db.session.add(prod)
+
+        db.session.commit()
 
         c1 = Comment(content='good', product_id=1, user_id=1)
         c2 = Comment(content='nice', product_id=1, user_id=1)
